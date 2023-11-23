7b5e572dc0b1f051cbbfdbf9c2c6c8f91e8530cb
joe-egan <joe.egan.23@ucl.ac.uk>
2023-11-23 09:51:59 +0000

    Adds CLI for numbers argument

--- squares.py
+++ squares.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser 
 """Computation of weighted average of squares."""
 
 
@@ -51,10 +52,14 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
+    parser = ArgumentParser(description="To pass numbers from command line")
+    parser.add_argument('numbers',nargs='*')
+    arguments = parser.parse_args()
+
+    # numbers_strings = ["1","2","4"]
     weight_strings = ["1","1","1"]        
     
-    numbers = convert_numbers(numbers_strings)
+    numbers = convert_numbers(arguments.numbers)
     weights = convert_numbers(weight_strings)
     
     result = average_of_squares(numbers, weights)
